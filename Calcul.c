086d51526e6950544642337135fda049081bde82
IzemHamouchene <77283279+IzemHamouchene@users.noreply.github.com>
2022-12-02 10:59:22 +0100

    Update and rename Sum.c to Calcul.c

---
+++ Calcul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+   //===== Inputs
+   // Arg 1 : Nombre 1
+   // Arg 2 : Operation arithmètique
+   // Arg 3 : Nombre 2
+   
+   //==== Output
+   //Affichage le résultat sous cette forme
+   //printf("%d", XXXXX);
+   return 0;
+}
